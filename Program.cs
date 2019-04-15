3afae8abaa8b92c497511ac74d84433b169c2c82
JuanAndOnly24 <49501471+JuanAndOnly24@users.noreply.github.com>
2019-04-15 18:14:42 +0300

    Changed if_else if.

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ namespace GameProject
     {
         static void Main(string[] args)
         {
-            int playerHP1 = 2000;
+            double playerHP1 = 2000;
             int playerHP2 = 2000;
             int i;
-            int dmg;
+            double dmg;
             int x;
             int power;
 
@@ -27,121 +27,60 @@ namespace GameProject
                         {
                             power = DmgGenerator.Next(150, 401);
                             x = PercentageGen.Next(-15, 61);
-                            if (x <= 0)
-                            {
                                 dmg = power - (power * x / 100);
                                 playerHP1 = playerHP1 - dmg;
                                 Console.WriteLine("You Did -" + dmg + "-DMG to the opponent with Right Hand Punch.");
                                 Console.WriteLine("Opponents life is: " + playerHP1 + ".");
-                            }
-                            else if (x > 0)
-                            {
-                                dmg = power + (power * x / 100);
-                                playerHP1 = playerHP1 - dmg;
-                                Console.WriteLine("You Did -" + dmg + "-DMG to the opponent with Right Hand Punch.");
-                                Console.WriteLine("Opponents life is: " + playerHP1 + ".");
-                            }
                             break;
                         }
                     case 2:
                         {
                             power = DmgGenerator.Next(90, 251);
                             x = PercentageGen.Next(-15, 61);
-                            Console.WriteLine(x);
-                            if (x <= 0)
-                            {
                                 dmg = power - (power * x / 100);
                                 playerHP1 = playerHP1 - dmg;
                                 Console.WriteLine("You Did -" + dmg + "-DMG to the opponent with Left Hand Punch.");
                                 Console.WriteLine("Opponents life is: " + playerHP1 + ".");
-                            }
-                            else if (x > 0)
-                            {
-                                dmg = power + (power * x / 100);
-                                playerHP1 = playerHP1 - dmg;
-                                Console.WriteLine("You Did -" + dmg + "-DMG to the opponent with Left Hand Punch.");
-                                Console.WriteLine("Opponents life is: " + playerHP1 + ".");
-                            }
                             break;
                         }
                     case 3:
                         {
                             power = DmgGenerator.Next(70, 321);
                             x = PercentageGen.Next(-15, 61);
-                            if (x <= 0)
-                            {
                                 dmg = power - (power * x / 100);
                                 playerHP1 = playerHP1 - dmg;
                                 Console.WriteLine("You Did -" + dmg + "-DMG to the opponent with Right Foot Kick.");
                                 Console.WriteLine("Opponents life is: " + playerHP1 + ".");
-                            }
-                            else if (x > 0)
-                            {
-                                dmg = power + (power * x / 100);
-                                playerHP1 = playerHP1 - dmg;
-                                Console.WriteLine("You Did -" + dmg + "-DMG to the opponent with Right Foot Kick.");
-                                Console.WriteLine("Opponents life is: " + playerHP1 + ".");
-                            }
                             break;
                         }
                     case 4:
                         {
                             power = DmgGenerator.Next(40, 201);
                             x = PercentageGen.Next(-15, 61);
-                            if (x <= 0)
-                            {
                                 dmg = power - (power * x / 100);
                                 playerHP1 = playerHP1 - dmg;
                                 Console.WriteLine("You Did -" + dmg + "-DMG to the opponent with Left Foot Kick.");
                                 Console.WriteLine("Opponents life is: " + playerHP1 + ".");
-                            }
-                            else if (x > 0)
-                            {
-                                dmg = power + (power * x / 100);
-                                playerHP1 = playerHP1 - dmg;
-                                Console.WriteLine("You Did -" + dmg + "-DMG to the opponent with Left Foot Kick.");
-                                Console.WriteLine("Opponents life is: " + playerHP1 + ".");
-                            }
                             break;
                         }
                     case 5:
                         {
                             power = DmgGenerator.Next(70, 301);
                             x = PercentageGen.Next(-15, 61);
-                            if (x <= 0)
-                            {
                                 dmg = power - (power * x / 100);
                                 playerHP1 = playerHP1 - dmg;
                                 Console.WriteLine("You Did -" + dmg + "-DMG to the opponent with Spin Foot Kick.");
                                 Console.WriteLine("Opponents life is: " + playerHP1 + ".");
-                            }
-                            else if (x > 0)
-                            {
-                                dmg = power + (power * x / 100);
-                                playerHP1 = playerHP1 - dmg;
-                                Console.WriteLine("You Did -" + dmg + "-DMG to the opponent with Spin Foot Kick.");
-                                Console.WriteLine("Opponents life is: " + playerHP1 + ".");
-                            }
                             break;
                         }
                     case 6:
                         {
                             power = DmgGenerator.Next(90, 351);
                             x = PercentageGen.Next(-15, 61);
-                            if (x <= 0)
-                            {
                                 dmg = power - (power * x / 100);
                                 playerHP1 = playerHP1 - dmg;
                                 Console.WriteLine("You Did -" + dmg + "-DMG to the opponent with Uppercut.");
                                 Console.WriteLine("Opponents life is: " + playerHP1 + ".");
-                            }
-                            else if (x > 0)
-                            {
-                                dmg = power + (power * x / 100);
-                                playerHP1 = playerHP1 - dmg;
-                                Console.WriteLine("You Did -" + dmg + "-DMG to the opponent with Uppercut.");
-                                Console.WriteLine("Opponents life is: " + playerHP1 + ".");
-                            }
                             break;
                         }
 
